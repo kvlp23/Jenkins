a579c9fbd5b645ebd8d3abbaf6d7013109527fc9
Pras <prasnuka@gmail.com>
2023-06-12 14:23:23 -0700

    jira 1256 first time project

---
+++ src/main/java/uk/pras/maven/jenkins/Application.java
@@ -0,0 +1,7 @@
+package uk.pras.maven.jenkins;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Maven is cool");
+    }
+}
---
+++ src/test/java/uk/pras/maven/jenkins/test/Test1.java
@@ -0,0 +1,23 @@
+package uk.pras.maven.jenkins.test;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+    @Test
+    public void test4(){
+        System.out.println("Running test 4");
+    }
+}
